2237e7c4d6d29f14580ffa8b80f840b63a64ac4d
Tai-Rocha <taina013@gmail.com>
2021-04-15 02:54:59 -0300

    SSP4_RCP6

---
+++ scripts/SSP4_RCP6/01_get_forest_classes.R
@@ -0,0 +1,120 @@
+###############################################################################
+## Script to manage GCAM land use dataset for SSP4_RCP6 (2020, 2030 and 2050)
+## Here we select and sum just forest classes
+## Author: Tainá Rocha
+## Data: 14 April 2021
+###############################################################################
+
+## Library
+
+library(raster)
+library(ncdf4)
+library(rgdal)
+library(terra)
+
+############################################### SSP4_RCP6 ############################################## 
+
+###############################################    2020    ############################################### 
+
+
+## Open netCFD file (ncdf4 packg)
+
+a_2020 <- nc_open('./data/GCAM/RAW/93d4aa096b15491b1ba136b46d8063cdca59d253c75d59791b4d4cb6f8a1ae91/Project ID 68344/GCAM-Demeter/GCAM-Harmonized/Mean_Std/SSP4_RCP6/GCAM_Demeter_LU_H_ssp4_rcp60_modelmean_2020.nc')
+
+## Convert variables in spatial objet (terra pckg)
+
+all_2020 <- rast(a_2020$filename)
+
+# Select onle forest classes (PTF1 to PTF8)
+
+forest_2020_1 <- all_2020[[2:9]]
+
+plot(forest_2020_1)
+
+## Sum forest classes
+
+Forest_2020 <- sum(forest_2020_1)
+
+plot(Forest_2020)
+
+
+## Save raster in results folder
+
+writeRaster(Forest_2020, "./data/GCAM/Forest_classes/SSP4_RCP6/2020/Forest_2020_1.tif")
+
+## Clean environment and plotslist all environment objects and remove
+rm(list=ls()) 
+
+## Remove all plots
+
+dev.off(dev.list()["RStudioGD"]) 
+
+###############################################    2030    ############################################### 
+
+## Open netCFD file (ncdf4 packg)
+
+b_2030 <- nc_open('./data/GCAM/RAW/93d4aa096b15491b1ba136b46d8063cdca59d253c75d59791b4d4cb6f8a1ae91/Project ID 68344/GCAM-Demeter/GCAM-Harmonized/Mean_Std/SSP4_RCP6/GCAM_Demeter_LU_H_ssp4_rcp60_modelmean_2030.nc')
+
+## Convert variables in spatial objet (terra pckg)
+
+all_2030 <- rast(b_2030$filename)
+
+# Select onle forest classes (PTF1 to PTF8)
+
+forest_2030_1 <- all_2030[[2:9]]
+
+plot(forest_2030_1)
+
+## Sum forest classes
+
+Forest_2030 <- sum(forest_2030_1)
+
+plot(Forest_2030)
+
+
+## Save raster in results folder
+
+writeRaster(Forest_2030, "./data/GCAM/Forest_classes/SSP4_RCP6/2030/Forest_2030_1.tif")
+
+## Clean environment and plotslist all environment objects and remove
+rm(list=ls()) 
+
+## Remove all plots
+
+dev.off(dev.list()["RStudioGD"]) 
+
+###############################################    2050    ############################################### 
+
+## Open netCFD file (ncdf4 packg)
+
+c_2050 <- nc_open('./data/GCAM/RAW/93d4aa096b15491b1ba136b46d8063cdca59d253c75d59791b4d4cb6f8a1ae91/Project ID 68344/GCAM-Demeter/GCAM-Harmonized/Mean_Std/SSP4_RCP6/GCAM_Demeter_LU_H_ssp4_rcp60_modelmean_2050.nc')
+
+## Convert variables in spatial objet (terra pckg)
+
+all_2050 <- rast(c_2050$filename)
+
+# Select onle forest classes (PTF1 to PTF8)
+
+forest_2050_1 <- all_2050[[2:9]]
+
+plot(forest_2050_1)
+
+## Sum forest classes
+
+Forest_2050 <- sum(forest_2050_1)
+
+plot(Forest_2050)
+
+
+## Save raster in results folder
+
+writeRaster(Forest_2050, "./data/GCAM/Forest_classes/SSP4_RCP6/2050/Forest_2050_1.tif")
+
+## Clean environment and plots
+
+rm(list=ls()) ## list all environment objects and remove
+
+## Remove all plots
+
+dev.off(dev.list()["RStudioGD"]) 
+
---
+++ scripts/SSP4_RCP6/02_histograms.R
@@ -0,0 +1,165 @@
+###################################################
+## Script to build histograms for forest (GCAM)
+## Here we select and sum just forest classes
+## Author: Tainá Rocha
+## Data: 14 April 2021
+###################################################
+
+#Library
+library(raster)
+library(ncdf4)
+library(rgdal)
+library(ggplot2)
+
+############################################### SSP4_RCP6 ############################################## 
+
+###############################################    2020    ############################################### 
+
+F_2020 <- raster("./data/GCAM/Forest_classes/SSP4_RCP6/2020/Forest_2020_1.tif")
+
+Forest_2020 <- t(flip(F_2020, direction = "y"))
+
+plot(Forest_2020)
+
+## Save final raster of forest
+
+writeRaster(Forest_2020, "./results/SSP4_RCP6_Forest_2020.tif")
+
+#####  Remove 0 values
+
+values(Forest_2020)[values(Forest_2020) <= 0] = NA
+
+plot(Forest_2020)
+
+# Histogram
+
+a <- hist(Forest_2020, breaks=30)
+ahist <- data.frame(counts= a$counts,breaks = a$mids)
+ggplot(ahist, aes(x = breaks, y = counts)) + 
+  geom_bar(stat = "identity",fill='black',alpha = 0.8)#+
+  #xlab("Forest Values")+ ylab("Frequency")+
+  #scale_x_continuous(breaks = seq(-1,1,0.25),  ## without this you will get the same scale
+                    # labels = seq(-1,1,0.25))    ## as hist (question picture)
+
+tiff(file="SSP4_RCP6_2020_Histogram.tiff",
+     width=9, height=7, units="in", res=150)
+ggplot(ahist, aes(x = breaks, y = counts, fill = breaks)) + ## Note the new aes fill here
+  geom_bar(stat = "identity",alpha = 0.8)+
+  xlab("Forest Values")+ ylab("Frequency")+
+  #scale_x_continuous(breaks = seq(0,1,10),
+                    # labels = seq(0,1,10))+
+  scale_fill_gradient(low="black", high="green")  
+
+dev.off()
+
+# Defalut R histogram
+#hist(Forest_2020,
+     #main = "Forest 2020 SSP1_RCP26",
+     #xlab = "Forest values", ylab = "Frequency",
+     #col = "springgreen")
+
+## Clean environment and plots
+
+rm(list=ls()) ## list all environment objects and remove
+
+## Remove all plots
+
+dev.off(dev.list()["RStudioGD"]) 
+
+###############################################    2030    ############################################### 
+
+F_2030 <- raster("./data/GCAM/Forest_classes/SSP4_RCP6/2030/Forest_2030_1.tif")
+
+Forest_2030 <- t(flip(F_2030, direction = "y"))
+
+plot(Forest_2030)
+
+## Save final raster of forest
+
+writeRaster(Forest_2030, "./results/SSP4_RCP6_Forest_2030.tif")
+
+#####  Remove 0 values
+
+values(Forest_2030)[values(Forest_2030) <= 0] = NA
+
+plot(Forest_2030)
+
+# Histogram
+
+b <- hist(Forest_2030, breaks=30)
+bhist <- data.frame(counts= b$counts,breaks = b$mids)
+ggplot(bhist, aes(x = breaks, y = counts)) + 
+  geom_bar(stat = "identity",fill='black',alpha = 0.8)#+
+#xlab("Forest Values")+ ylab("Frequency")+
+#scale_x_continuous(breaks = seq(-1,1,0.25),  ## without this you will get the same scale
+# labels = seq(-1,1,0.25))    ## as hist (question picture)
+
+tiff(file="SSP4_RCP6_2030_Histogram.tiff",
+     width=9, height=7, units="in", res=150)
+ggplot(bhist, aes(x = breaks, y = counts, fill = breaks)) + ## Note the new aes fill here
+  geom_bar(stat = "identity",alpha = 0.8)+
+  xlab("Forest Values")+ ylab("Frequency")+
+  #scale_x_continuous(breaks = seq(0,1,10),
+  # labels = seq(0,1,10))+
+  scale_fill_gradient(low="black", high="green")  
+
+dev.off()
+
+## Clean environment and plots
+
+rm(list=ls()) ## list all environment objects and remove
+
+## Remove all plots
+
+dev.off(dev.list()["RStudioGD"]) 
+
+
+###############################################    2050    ############################################### 
+
+F_2050 <- raster("./data/GCAM/Forest_classes/SSP4_RCP6/2050/Forest_2050_1.tif")
+
+Forest_2050 <- t(flip(F_2050, direction = "y"))
+
+plot(Forest_2050)
+
+## Save final raster of forest
+
+writeRaster(Forest_2050, "./results/SSP4_RCP6_Forest_2050.tif")
+
+#####  Remove 0 values
+
+values(Forest_2050)[values(Forest_2050) <= 0] = NA
+
+plot(Forest_2050)
+
+# Histogram
+
+c <- hist(Forest_2050, breaks=30)
+chist <- data.frame(counts= c$counts,breaks = c$mids)
+ggplot(chist, aes(x = breaks, y = counts)) + 
+  geom_bar(stat = "identity",fill='black',alpha = 0.8)#+
+#xlab("Forest Values")+ ylab("Frequency")+
+#scale_x_continuous(breaks = seq(-1,1,0.25),  ## without this you will get the same scale
+# labels = seq(-1,1,0.25))    ## as hist (question picture)
+
+tiff(file="SSP4_RCP6_2050_Histogram.tiff",
+     width=9, height=7, units="in", res=150)
+ggplot(chist, aes(x = breaks, y = counts, fill = breaks)) + ## Note the new aes fill here
+  geom_bar(stat = "identity",alpha = 0.8)+
+  xlab("Forest Values")+ ylab("Frequency")+
+  #scale_x_continuous(breaks = seq(0,1,10),
+  # labels = seq(0,1,10))+
+  scale_fill_gradient(low="black", high="green")  
+
+dev.off()
+
+## Clean environment and plots
+
+rm(list=ls()) ## list all environment objects and remove
+
+## Remove all plots
+
+dev.off(dev.list()["RStudioGD"]) 
+
+
+
