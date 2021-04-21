e189c1e19a7eda079717f89076dac9475c8994a5
Tai-Rocha <taina013@gmail.com>
2021-04-21 17:14:56 -0300

    testing...

---
+++ scripts/test.R
@@ -0,0 +1,53 @@
+
+
+
+library(raster)
+
+## Ok
+lista <- list.files(path = "./results/", pattern = ".tif", recursive = T,  full.names = T) # list 12 layers
+
+a <- stack(lista) # put together and plot
+plot(a)
+
+b <- a >=0.1 # make binary with treshold 0.1
+plot(b)
+
+## Work, but  create a folder ( in parental folder) for each file as follow :
+#[1] "SSP5_RCP85_Forest_2050/SSP5_RCP85_Forest_2050.tif"
+#[1] "SSP5_RCP85_Forest_2050/SSP5_RCP85_Forest_2050.tif"
+#[1] "SSP5_RCP85_Forest_2050/SSP5_RCP85_Forest_2050.tif"
+#[1] "SSP5_RCP85_Forest_2050/SSP5_RCP85_Forest_2050.tif"
+#[1] "SSP5_RCP85_Forest_2050/SSP5_RCP85_Forest_2050.tif"
+#[1] "SSP5_RCP85_Forest_2050/SSP5_RCP85_Forest_2050.tif". 
+
+
+nms <- names(a) # correct names
+
+for (i in 1:nlayers(b)) {
+  writeRaster(b, filename = file.path(nms[i], paste0(nms[i], ".tif")), )
+  #dir.create(nms[i], F, F)
+  print(filename)
+}
+
+# I need a folder per SSP, where I have 3 files correpondin a years 20020, 2030 and 2050. 
+
+## Not run, not work
+
+b <- paste0(names(binary_01@data),"_bin.tif")
+
+lapply(binary_01, FUN=function(x) {
+  writeRaster(x, file.path("./results/","train", paste0(names(x), ".tif")), )
+})
+  
+##
+
+c <- paste0(names(binary_01@data),"_cont.tif")
+
+lapply(cont_01, FUN=function(x) {
+  writeRaster(x, file.path("./results/","train", paste0(names(c), ".tif")), )
+})
+
+
+
+
+
