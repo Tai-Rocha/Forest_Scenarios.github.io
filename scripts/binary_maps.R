86fc088d71f996389b36690650b905d264ae32cb
Tai-Rocha <taina013@gmail.com>
2021-04-23 17:17:49 -0300

    Script finished. But need to decide the best threshold to aplly

---
+++ scripts/binary_maps.R
@@ -0,0 +1,30 @@
+#################################################################################################
+## Forests in SSPs scenarios 
+## Aplly a minimmun tresholg of 0.1 to make binary raster (0- No forest and 1- Forest)
+## Author: Tainá Rocha
+## Date: 23 April  2021
+#################################################################################################
+
+# Library
+library(raster)
+
+## Steps
+
+#1 list 12 layers
+
+lista <- list.files(path = "./results/", pattern = ".tif", recursive = T,  full.names = T)  
+
+#2 put together and plot
+a <- stack(lista) 
+plot(a)
+
+#3 make binary with treshold 10
+b <- a >50.0
+plot(b)
+
+#4 correct names
+names(b) <-names(a)
+
+#5 Wite rasters
+writeRaster(b, filename = names(a), format = "GTiff", bylayer = T)
+
